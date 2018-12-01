40c62af6ddfca798d2b74932cf1717fe6ba122d8
samuelhegner <C16439262@mydit.ie>
2018-12-01 12:41:52 +0000

    Cosmos Start (Samuel Hegner)

---
+++ Imagine_Protoype_Project/Assets/Animate_Trail.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animate_Trail : MonoBehaviour
+{
+
+    Gradient grad;
+
+    void Start()
+    {
+        grad = GetComponent<LineRenderer>().colorGradient;
+    }
+
+    void Update()
+    {
+        
+    }
+}
