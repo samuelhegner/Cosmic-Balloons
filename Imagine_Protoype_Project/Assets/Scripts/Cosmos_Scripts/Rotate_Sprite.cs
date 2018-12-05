4fe590d51013cdd7f7881769894fa632b8a61d3c
gazor117 <oreillygary7@gmail.com>
2018-12-05 16:30:30 +0000

    Galaxy Stars trail and effect

---
+++ Imagine_Protoype_Project/Assets/Scripts/Cosmos_Scripts/Rotate_Sprite.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Sprite : MonoBehaviour
+{
+
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Awake()
+    {
+
+        int ran = Random.Range(0, 2);
+        if (ran < 1)
+        {
+            rotateSpeed *= -1;
+        }
+        else
+        {
+            rotateSpeed *= 1;
+        }
+    }
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate( (Vector3.forward * rotateSpeed));   
+    }
+}
