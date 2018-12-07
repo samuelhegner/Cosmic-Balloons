657c358a3b8e50753215ed83f42167f0b8db8fc6
gazor117 <oreillygary7@gmail.com>
2018-12-07 14:38:51 +0000

    Circle Effect

---
+++ Imagine_Protoype_Project/Assets/Scripts/Menu_Scripts/Restart_Circle_Effect.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Restart_Circle_Effect : MonoBehaviour
+{
+    public float waitTime = 0;
+    public float delay;
+    private ParticleSystem ps;
+    private float timeremaining;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        timeremaining = waitTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        delay -= Time.deltaTime;
+        var emission = ps.emission;
+        
+        if (delay < 0)
+        {
+            
+            timeremaining -= Time.deltaTime;
+            emission.enabled = true;
+        
+            if (timeremaining < 0)
+            {
+                ps.Play();
+                timeremaining = waitTime;
+            } 
+        }
+        
+       
+    }
+}
